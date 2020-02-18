a9e354d49a83e1e3f7425436ed935c2f19a7e3cd
Rakesh Kirola <55341835+urakesh661@users.noreply.github.com>
2020-02-18 21:04:37 +0530

    Update robots_file_analyzer.sh

--- robots_file_analyzer.sh
+++ robots_file_analyzer.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-start_time=$(date +%s)
-
 rm -rf robotfile_path.txt
 
 echo -n "Please provide a domain: "
@@ -13,9 +11,23 @@ if [ -z $domain ];then
 	exit
 fi
 
-if wget  -SO- --timeout=1 --tries=1 -q $domain/robots.txt 2>/dev/null | egrep -i "Disallow"| awk '{print $2}' >>robotfile_path.txt;then
+echo -n "Please provide timeout[2]: "
+read wgettimeout
+
+echo -n "Please provide tries[2]: "
+read maxretries
+
+echo -n "Please provide connect-timeout[2]: "
+read connecttimeout
+
+echo -n "Please provide max-time[2]: "
+read maxtime
+
+start_time=$(date +%s)
+
+if wget  -SO- --timeout=${wgettimeout:-2} --tries=${maxretries:-2} -q $domain/robots.txt 2>/dev/null | egrep -i "Disallow"| awk '{print $2}' >>robotfile_path.txt;then
 	for i in $(cat robotfile_path.txt);do
-		curl --silent --insecure --write-out "%{http_code} %{url_effective}\n" --connect-timeout 4 --max-time 4     $domain$i https://$domain$i --output /dev/null
+		curl --silent --insecure --write-out "%{http_code} %{url_effective}\n" --connect-timeout ${connecttimeout:-2} --max-time ${maxtime:-2} $domain$i https://$domain$i --output /dev/null
 	done
 else
 	echo "Robots.txt file doesn't exist"
